3ee9d825c91ebe7b50d6b6058bef02d5a8eadcb3
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2022-12-13 01:44:49 +0200

    scale

--- src/utils/config.gd
+++ src/utils/config.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var animation_speed = 2.5
+var zoom_const = .5
--- src/utils/draggable.gd
+++ src/utils/draggable.gd
@@ -1,8 +1,9 @@
 extends Spatial
-class_name Draggable
+class_name Draggable # + Scalable
 
 var is_rotating = false # GD4 doesnt like the keyword rotating
 var last_position = Vector2()
+var new_scale: Vector3 = scale
 
 func _process(delta):
 	if (Input.is_action_just_pressed("Rotate")):
@@ -16,3 +17,19 @@ func _process(delta):
 		last_position = get_viewport().get_mouse_position()
 		self.rotate_y(dif.x * 1 * delta)
 		self.rotate_x(dif.y * 1 * delta)
+	
+	if (new_scale.x < 0 || new_scale.y < 0 || new_scale.z < 0):
+		new_scale = Vector3.ZERO
+	scale = lerp(scale, new_scale, .1)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			# zoom in
+			if event.button_index == BUTTON_WHEEL_UP:
+				new_scale = Vector3(scale.x + Config.zoom_const, scale.y + Config.zoom_const, scale.z + Config.zoom_const)
+				# call the zoom function
+			# zoom out
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				new_scale = Vector3(scale.x - Config.zoom_const, scale.y - Config.zoom_const, scale.z - Config.zoom_const)
+				# call the zoom function
